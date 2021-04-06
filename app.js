0fec2fc7ed66ae144f93a72235c06b548b39e069
Felix Thissen <felixthissen@gmail.com>
2021-04-06 12:32:11 +0200

    Ich habe die Farben etwas heller gemacht. Portfolio und FAQ sind angelegt. Alle Seiten im Grund Layout sind angelegt. Kleine Änderungen, zum Beispiel max-width von Texten

--- app.js
+++ app.js
@@ -25,3 +25,18 @@ const navSlide = () => {
 }
 
 navSlide();
+
+//COOKIE CONTAINER
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
